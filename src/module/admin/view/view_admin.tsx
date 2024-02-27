bdad06e6bf2a6e7425bd4bacae1950810ea91bed
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-27 17:32:35 +0800

    style : admin
    Deskripsi:
    - update admin
    
    No Issue

--- src/module/admin/view/view_admin.tsx
+++ src/module/admin/view/view_admin.tsx
@@ -1,10 +1,22 @@
+import { Warna } from '@/module/_global';
+import { Box, Button, Group, Text } from '@mantine/core';
 import React from 'react';
+import TableAdmin from '../components/table_admin';
+import { FiFolderPlus } from 'react-icons/fi';
 
 export default function ViewAdmin() {
   return (
-    <div>
-      ViewAdmin
-    </div>
+    <>
+      <Group justify='space-between'>
+        <Box>
+          <Text fz={30} c={Warna.biruTua}>ADMIN</Text>
+        </Box>
+        <Button radius={"md"} leftSection={<FiFolderPlus />}>Create</Button>
+      </Group>
+      <Box pt={20}>
+        <TableAdmin />
+      </Box>
+    </>
   );
 }
 
