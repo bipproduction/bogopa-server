3ba852236aeceb3954e85f163749797a19dcd317
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-27 17:34:49 +0800

    style : role
    Deskripsi:
    - update role
    
    NO Issue

--- src/module/role/view/view_role.tsx
+++ src/module/role/view/view_role.tsx
@@ -1,10 +1,22 @@
+import { Box, Button, Group, Text } from '@mantine/core';
 import React from 'react';
+import TableRole from '../components/table_role';
+import { Warna } from '@/module/_global';
+import { FiFolderPlus } from 'react-icons/fi';
 
 export default function ViewRole() {
   return (
-    <div>
-      ViewRole
-    </div>
+    <>
+      <Group justify='space-between'>
+      <Box>
+        <Text fz={30} c={Warna.biruTua}>ROLE</Text>
+      </Box>
+        <Button  radius={"md"} leftSection={<FiFolderPlus/>}>Create</Button>
+      </Group>
+      <Box pt={20}>
+        <TableRole />
+      </Box>
+    </>
   );
 }
 
