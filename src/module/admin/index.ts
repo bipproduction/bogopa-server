d2f8fcac587445b4c8ef181ab981e1d7b6a0d199
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-29 12:26:26 +0800

    style : admin
    Deskripsi:
    - update admin
    
    No Issue

--- src/module/admin/index.ts
+++ src/module/admin/index.ts
@@ -1,2 +1,4 @@
 import ViewAdmin from "./view/view_admin"
+import ViewAddAdmin from "./view/view_add_admin"
 export {ViewAdmin}
+export {ViewAddAdmin}
--- src/module/admin/view/view_admin.tsx
+++ src/module/admin/view/view_admin.tsx
@@ -1,17 +1,20 @@
+'use client'
 import { Warna } from '@/module/_global';
 import { Box, Button, Group, Text } from '@mantine/core';
 import React from 'react';
 import TableAdmin from '../components/table_admin';
 import { FiFolderPlus } from 'react-icons/fi';
+import { useRouter } from 'next/navigation';
 
 export default function ViewAdmin() {
+  const router = useRouter()
   return (
     <>
       <Group justify='space-between'>
         <Box>
           <Text fz={30} c={Warna.biruTua}>ADMIN</Text>
         </Box>
-        <Button radius={"md"} leftSection={<FiFolderPlus />}>Create</Button>
+        <Button radius={"md"} leftSection={<FiFolderPlus />} onClick={() => router.push("/admin/add")} >Create</Button>
       </Group>
       <Box pt={20}>
         <TableAdmin />
