21e1daa91243542029b3a6535bb9375aec155538
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-29 16:27:53 +0800

    style : transaksi
    Deskripsi:
    - update transaksi
    
    No Issue

---
+++ src/module/transaksi/components/echart_transaksi.tsx
@@ -0,0 +1,98 @@
+'use client'
+import { EChartsOption, color } from "echarts";
+import EChartsReact from "echarts-for-react";
+import * as echarts from 'echarts';
+import { useState } from "react";
+import { useShallowEffect } from "@mantine/hooks";
+import { Box } from "@mantine/core";
+import { Warna } from "@/module/_global";
+
+
+export default function EchartTransaksi() {
+  const [options, setOptions] = useState<EChartsOption>({})
+
+  useShallowEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = () => {
+    const option: EChartsOption = {
+      title: {
+        text: 'TRANSAKSI PERHARI',
+        textStyle: {
+          fontSize: 20,
+          color: Warna.pink
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['2023-10-16', '2023-10-17', '2023-10-18', "2023-10-19", "2023-10-20", "2023-10-21", "2023-10-22", "2023-10-23"],
+          boundaryGap: false,
+          axisLabel: {
+            verticalAlign: "middle",
+            rotate: 47,
+            color: "dark"
+          },
+        },
+
+      ],
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+
+          color: "dark"
+        },
+
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      series: [
+        {
+          data: [23, 54, 42, 16, 53, 81, 53, 32],
+          type: 'line',
+          color: Warna.pink,
+          showSymbol: false,
+          areaStyle: {
+            opacity: 1,
+            color: new echarts.graphic.LinearGradient(1, 1, 1, 0, [
+              {
+                offset: 0,
+                color: 'white'
+              },
+              {
+                offset: 1,
+                color: Warna.pink
+              }
+            ])
+          },
+
+        }
+      ]
+    }
+    setOptions(option);
+  }
+
+  return (
+    <>
+      <Box>
+        <Box style={{
+          border: `1px solid ${Warna.warnaBorder}`,
+          borderRadius: 10,
+          boxShadow: "1px 1px 5px 0px rgba(0,0,0,0.10)",
+        }} p={0}>
+          <Box p={10}>
+            <EChartsReact style={{ height: 300, }} option={options} />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
+}
