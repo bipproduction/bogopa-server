ac4180bd8a2695a4f430406fce31b4abb280e515
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-29 12:26:49 +0800

    style : gift
    Deskripsi:
    - update gift
    
    No Issue

--- src/module/gift/components/table_gift.tsx
+++ src/module/gift/components/table_gift.tsx
@@ -1,82 +1,63 @@
 'use client'
 import { Warna } from '@/module/_global';
-import { ActionIcon, Box, Button, Center, Group, Pagination, Switch, Table, Text } from '@mantine/core';
+import { ActionIcon, Avatar, Box, Button, Center, Group, Pagination, Switch, Table, Text } from '@mantine/core';
 import React from 'react';
-import {AiOutlineFileSearch} from 'react-icons/ai'
-import {LiaEditSolid} from 'react-icons/lia'
-import {FiFolderPlus} from 'react-icons/fi'
+import { AiOutlineFileSearch } from 'react-icons/ai'
+import { LiaEditSolid } from 'react-icons/lia'
+import { FiFolderPlus } from 'react-icons/fi'
+import { useRouter } from 'next/navigation';
 
 export default function TableGift() {
   const User = [
     {
       id: 1,
-      name: 'Doni',
-      price: '32.000'
+      name: 'Mawar',
+      image: 'https://i.pravatar.cc/200?img=4',
+      price: '6.000',
+      tgl: '27, Februari 2024',
     },
     {
       id: 2,
-      name: 'Roni',
-      price: '200.000'
+      name: 'Topi',
+      image: 'https://i.pravatar.cc/200?img=4',
+      price: '10.000',
+      tgl: '11, Februari 2024',
     },
     {
       id: 3,
-      name: 'Risma',
-      price: '50.000'
+      name: 'Singa',
+      image: 'https://i.pravatar.cc/200?img=4',
+      price: '1.050.000',
+      tgl: '12, Februari 2024',
     },
     {
       id: 4,
-      name: 'Desi',
-      price: '62.000'
-    },
-    {
-      id: 6,
-      name: 'Angga',
-      price: '80.000'
-    },
-    {
-      id: 7,
-      name: 'Dwi',
-      price: '120.000'
-    },
-    {
-      id: 5,
-      name: 'Lisa',
-      price: '890.000'
-    },
-    {
-      id: 8,
-      name: 'Dona',
-      price: '100.000'
-    },
-    {
-      id: 9,
-      name: 'Linda',
-      price: '20.000'
-    },
-    {
-      id: 10,
-      name: 'Ilham',
-      price: '15.000'
+      name: 'Moyet',
+      image: 'https://i.pravatar.cc/200?img=4',
+      price: '362.000',
+      tgl: '12, Februari 2024',
     },
   ]
+
+  const router = useRouter()
+
   return (
     <>
-    <Box style={{
+      <Box style={{
         border: `1px solid ${Warna.warnaBorder}`,
         borderRadius: 10,
         boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.10)",
       }}>
         <Box p={20}>
-        <Table  >
+          <Table  >
             <Table.Thead >
               <Table.Tr >
-                <Table.Th>NO</Table.Th>
                 <Table.Th>NAMA</Table.Th>
-                <Table.Th>PRICE</Table.Th>
+                <Table.Th>HARGA</Table.Th>
                 <Table.Th>NON AKTIF</Table.Th>
                 <Table.Th>
                   <Center>
-                  ACTION
+                    ACTION
                   </Center>
                 </Table.Th>
               </Table.Tr>
@@ -84,22 +65,27 @@ export default function TableGift() {
             <Table.Tbody  >
               {User.map((v, i) => (
                 <Table.Tr key={i}>
-                  <Table.Td>{i + 1}</Table.Td>
-                  <Table.Td>{v.name}</Table.Td>
+
+                  <Table.Td>
+                    <Group>
+                      <Avatar style={{ cursor: "pointer" }} src={v.image} size='lg' mr={2} onClick={() => router.push('/gift/' + v.id)} />
+                      <Box>
+                        <Text fw={"bold"}>{v.name}</Text>
+                        <Text c={"gray"} fz={14}>{v.tgl}</Text>
+                      </Box>
+                    </Group>
+                  </Table.Td>
                   <Table.Td >{v.price}</Table.Td>
                   <Table.Td >
-                  <Switch size="lg" onLabel="AKTIF" offLabel="NON AKTIF" />
+                    <Switch size="lg" onLabel="AKTIF" offLabel="NON AKTIF" />
                   </Table.Td>
                   <Table.Td >
                     <Center>
-                    <Group>
-                      <ActionIcon variant="subtle">
-                      <LiaEditSolid size={30}/>
-                      </ActionIcon>
-                      <ActionIcon variant="subtle">
-                      <AiOutlineFileSearch size={30}/>
-                      </ActionIcon>
-                    </Group>
+                      <Group>
+                        <ActionIcon variant="subtle">
+                          <LiaEditSolid size={30} />
+                        </ActionIcon>
+                      </Group>
                     </Center>
                   </Table.Td>
                 </Table.Tr>
@@ -108,9 +94,9 @@ export default function TableGift() {
           </Table>
         </Box>
       </Box>
-        <Group justify='flex-end' pt={20}>
+      <Group justify='flex-end' pt={20}>
         <Pagination total={10} />
-        </Group>
+      </Group>
     </>
   );
 }
