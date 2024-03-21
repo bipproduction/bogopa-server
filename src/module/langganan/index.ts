e102dadd8f1958e3718f9f53389cfa682d943cc3
amel <amel@amel.local>
2024-03-21 15:36:45 +0800

    upd : koneksi page langganan part 1
    Deskripsi:
    - list langganan
    - otw add langganan
    
    No Issues

--- src/module/langganan/index.ts
+++ src/module/langganan/index.ts
@@ -2,8 +2,10 @@ import ViewLangganan from "./view/view_langganan";
 import ViewAddLangganan from "./view/view_add_langganan";
 import ViewEditLangganan from "./view/view_edit_langganan";
 import ViewDetailLangganan from "./view/view_detail_langganan";
+import funGetListLangganan from "./fun/get_list_langganan";
 
 export { ViewLangganan }
 export { ViewAddLangganan }
 export { ViewEditLangganan }
 export { ViewDetailLangganan }
+export { funGetListLangganan }
--- src/module/langganan/view/view_langganan.tsx
+++ src/module/langganan/view/view_langganan.tsx
@@ -1,14 +1,34 @@
 'use client'
-import { Box, Button, Group, Input, Text } from '@mantine/core';
-import React from 'react';
+import { Box, Button, Group, Input, Pagination, Text } from '@mantine/core';
+import React, { useState } from 'react';
 import { Warna } from '@/module/_global';
 import { FiFolderPlus } from 'react-icons/fi';
 import { useRouter } from 'next/navigation';
 import { GoSearch } from 'react-icons/go';
 import TableLangganan from '../components/table_langganan';
+import { funGetListLangganan } from '..';
 
-export default function ViewLangganan() {
+export default function ViewLangganan({ data }: { data: any }) {
   const router = useRouter()
+  const [isData, setData] = useState(data.data)
+  const [isSearch, setSearch] = useState("")
+  const [isNPage, setNPage] = useState(data.nPage)
+  const [isChoosePage, setChoosePage] = useState(1)
+
+  async function onLoad(p: any) {
+    const load = await funGetListLangganan({ search: isSearch, page: isChoosePage })
+    setChoosePage(p)
+    setData(load.data)
+    setNPage(load.nPage)
+  }
+
+  async function onSearch(s: any) {
+    const load = await funGetListLangganan({ search: s, page: 1 })
+    setSearch(s)
+    setChoosePage(1)
+    setData(load.data)
+    setNPage(load.nPage)
+  }
   return (
     <>
       <Group >
@@ -18,10 +38,19 @@ export default function ViewLangganan() {
       </Group>
       <Box pt={20}>
         <Group pb={10} justify="space-between">
-          <Input radius={10} placeholder="Search by name, email or phone ..." leftSection={<GoSearch size={16} />} w={400} />
+          <Input radius={10} placeholder="Search by name" leftSection={<GoSearch size={16} />} w={400} />
           <Button radius={"md"} leftSection={<FiFolderPlus />} onClick={() => router.push('/langganan/add')}>Create</Button>
         </Group>
-        <TableLangganan />
+        <TableLangganan data={isData} />
+        <Group justify='flex-end' pt={20}>
+          <Pagination
+            value={isChoosePage}
+            onChange={(val) => {
+              onLoad(val)
+            }}
+            total={isNPage}
+          />
+        </Group>
       </Box>
     </>
   );
