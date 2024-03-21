69409cd95b5c6d3d0151e0fc8dfd6ba05137829f
amel <amel@amel.local>
2024-03-21 15:34:42 +0800

    upd: update penunjang
    Deskripsi;
    - install package rich text
    - update struktur database
    - update seeder langganan componen
    - update folder image
    
    No Issues

--- src/module/seeder/index.ts
+++ src/module/seeder/index.ts
@@ -2,9 +2,11 @@ import seederAdminRole from "./data/admin_role.json"
 import seederAdminUser from "./data/admin_user.json"
 import seederAdminComponent from "./data/admin_component.json"
 import seederAdminAccess from "./data/admin_access.json"
+import seederLanggananComponent from "./data/langganan_component.json"
 
 
 export { seederAdminRole }
 export { seederAdminUser }
 export { seederAdminComponent }
 export { seederAdminAccess }
+export { seederLanggananComponent }
